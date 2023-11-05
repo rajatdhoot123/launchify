59485a259278c15e344a7488dd1593122a84e79a
rajat <rajat@rajatmacbook.local>
2023-11-05 18:10:53 +0530

    Added Files to ignore in build file

--- post-build.js
+++ post-build.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 
+const IGNORE_FILES = [
+  "src/app/components/select",
+  "src/app/components/floater",
+];
+
 fs.cpSync("src/app/components", "uicomponents/src/app/components", {
+  filter: (file) => (IGNORE_FILES.includes(file) ? false : true),
   recursive: true,
 });
