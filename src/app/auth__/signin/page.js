779bba9f90ba2514abd2b8a0d68d7382a67cfb6b
Rajat <rajatdhoot123@gmail.com>
2023-11-08 17:23:26 +0530

    Pro Version in Making

---
+++ src/app/auth__/signin/page.js
@@ -0,0 +1,54 @@
+import GoogleButton from "@/app/auth/GoogleSignIn";
+import EmailSignIn from "@/app/auth__/signin/EmailSignIn";
+
+async function getProviders() {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/providers`);
+
+  if (!res.ok) {
+    console.log(res);
+    throw new Error("Failed to fetch providers");
+  }
+
+  return res.json();
+}
+
+export default async function SignIn() {
+  let resp;
+  try {
+    resp = (await getProviders()) || {};
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!resp) {
+    return null;
+  }
+
+  return (
+    <div
+      className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-blue-200 to-green-200 dark:from-gray-800 dark:to-gray-900"
+      data-id="1"
+    >
+      <div
+        className="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md w-full overflow-hidden relative"
+        data-id="2"
+      >
+        <h1 className="text-2xl font-bold text-center mb-5" data-id="4">
+          Login
+        </h1>
+        <GoogleButton />
+        <div
+          className="border-b border-gray-200 dark:border-gray-600 my-4"
+          data-id="7"
+        ></div>
+        <p
+          className="mb-6 text-sm text-gray-600 dark:text-gray-400"
+          data-id="8"
+        >
+          Or get a magic link sent to your email
+        </p>
+        <EmailSignIn />
+      </div>
+    </div>
+  );
+}
--- src/app/components/login/varient-2.jsx
+++ src/app/components/login/varient-2.jsx
@@ -1,18 +1,18 @@
 const Login = () => {
   return (
     <div
-      class="w-full h-screen flex items-center justify-center bg-gradient-to-r from-blue-200 to-green-200 dark:from-gray-800 dark:to-gray-900"
+      className="w-full h-screen flex items-center justify-center bg-gradient-to-r from-blue-200 to-green-200 dark:from-gray-800 dark:to-gray-900"
       data-id="1"
     >
       <div
-        class="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md w-full overflow-hidden relative"
+        className="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-md w-full overflow-hidden relative"
         data-id="2"
       >
-        <h1 class="text-2xl font-bold text-center mb-5" data-id="4">
+        <h1 className="text-2xl font-bold text-center mb-5" data-id="4">
           Login
         </h1>
         <button
-          class="rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full py-2 mb-4 flex items-center justify-center"
+          className="rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 w-full py-2 mb-4 flex items-center justify-center"
           data-id="5"
         >
           <svg
@@ -22,10 +22,10 @@ const Login = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class=" mr-2 text-red-500 h-5 w-5"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className=" mr-2 text-red-500 h-5 w-5"
             data-id="6"
           >
             <rect width="20" height="16" x="2" y="4" rx="2"></rect>
@@ -34,22 +34,22 @@ const Login = () => {
           Login with Google
         </button>
         <div
-          class="border-b border-gray-200 dark:border-gray-600 my-4"
+          className="border-b border-gray-200 dark:border-gray-600 my-4"
           data-id="7"
         ></div>
-        <p class="mb-6 text-sm text-gray-600 dark:text-gray-400" data-id="8">
+        <p className="mb-6 text-sm text-gray-600 dark:text-gray-400" data-id="8">
           Or get a magic link sent to your email
         </p>
-        <div class="mb-4" data-id="9">
+        <div className="mb-4" data-id="9">
           <label
-            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             for="email"
             data-id="10"
           >
             Email
           </label>
           <input
-            class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             id="email"
             placeholder="you@example.com"
             required=""
@@ -58,7 +58,7 @@ const Login = () => {
           />
         </div>
         <button
-          class="rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary-foreground hover:bg-gray-100 h-10 px-4 py-2 w-full flex items-center justify-center border border-gray-300"
+          className="rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-primary-foreground hover:bg-gray-100 h-10 px-4 py-2 w-full flex items-center justify-center border border-gray-300"
           type="submit"
           data-id="12"
         >
@@ -69,10 +69,10 @@ const Login = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class=" mr-2 text-blue-500 h-5 w-5"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className=" mr-2 text-blue-500 h-5 w-5"
             data-id="13"
           >
             <rect width="20" height="16" x="2" y="4" rx="2"></rect>
