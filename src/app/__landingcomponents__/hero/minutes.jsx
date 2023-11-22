1f0d90d8e8727bde15346f0ed537cf1b0c9cc11d
rajat <rajat@rajatmacbook.local>
2023-11-22 17:26:40 +0530

    Hydration fix

--- src/app/__landingcomponents__/hero/minutes.jsx
+++ src/app/__landingcomponents__/hero/minutes.jsx
@@ -1,9 +1,15 @@
 "use client";
+
+import { useEffect, useState } from "react";
+
 const Minutes = () => {
+  const [time, setTime] = useState();
+  useEffect(() => {
+    setTime(new Date(Date.now() + 5 * 60 * 1000).toLocaleTimeString());
+  }, []);
   return (
     <p className="text-lg font-medium leading-8 text-indigo-600/95">
-      Landing Page will be ready today at{" "}
-      {new Date(Date.now() + 5 * 60 * 1000).toLocaleTimeString()}
+      Landing Page will be ready today at {time}
     </p>
   );
 };
