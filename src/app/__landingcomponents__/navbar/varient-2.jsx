785f1efc54e7b4e0714b91255f839cbf53f86b3b
rajat <rajat@rajatmacbook.local>
2023-11-22 17:47:21 +0530

    Sign in Sign up button added

--- src/app/__landingcomponents__/navbar/varient-2.jsx
+++ src/app/__landingcomponents__/navbar/varient-2.jsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const NavBar = () => {
@@ -299,18 +300,18 @@ const NavBar = () => {
               </a>
             </nav>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a
-                href="#"
+              <Link
+                href="/auth/signin"
                 className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
               >
                 Sign in
-              </a>
-              <a
-                href="#"
+              </Link>
+              <Link
+                href="/auth/signin"
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 Sign up
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -540,17 +541,20 @@ const NavBar = () => {
                 </a>
               </div>
               <div>
-                <a
-                  href="#"
+                <Link
+                  href="/auth/signin"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Sign up
-                </a>
+                </Link>
                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                   Existing customer?
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                  <Link
+                    href="/auth/signin"
+                    className="text-indigo-600 hover:text-indigo-500"
+                  >
                     Sign in
-                  </a>
+                  </Link>
                 </p>
               </div>
             </div>
