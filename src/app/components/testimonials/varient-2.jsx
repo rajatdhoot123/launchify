0ad0676cdce4c31e78a4528b69eb6102ef055f45
rajat <rajat@rajatmacbook.local>
2023-11-22 11:07:17 +0530

    Testinomial Updated

--- src/app/components/testimonials/varient-2.jsx
+++ src/app/components/testimonials/varient-2.jsx
@@ -1,48 +1,51 @@
 /* eslint-disable react/no-unescaped-entities */
 const Testinomials = () => {
   return (
-    <section className="flex items-center justify-center py-16  min-w-screen">
-      <div className="max-w-6xl px-12 mx-auto  md:px-16 xl:px-10">
-        <div className="flex flex-col items-center lg:flex-row">
-          <div className="flex flex-col items-start justify-center w-full h-full pr-8 mb-10 lg:mb-0 lg:w-1/2">
+    <section className="px-5 md:px-0 container mx-auto">
+      <div className="max-w-6xl mx-auto  md:px-16 xl:px-10">
+        <div className="flex flex-col items-center lg:flex-row lg:gap-8 gap-0">
+          <div className="flex flex-col items-center lg:items-start justify-center w-full h-full mb-10 lg:mb-0 lg:w-1/2">
             <p className="mb-2 text-base font-medium tracking-tight text-indigo-500 uppercase">
               Our customers love our product
             </p>
             <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
               Testimonials
             </h2>
-            <p className="my-6 text-lg text-gray-600">
+            <p className="text-center lg:text-left my-6 text-lg text-gray-600">
               Don't just take our word for it, read from our extensive list of
               case studies and customer testimonials.
             </p>
             <a
               href="#_"
-              className="flex items-center justify-center px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md shadow hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10"
+              className="flex items-center justify-center px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md shadow focus:shadow-outline-indigo hover:bg-indigo-500 focus:outline-none focus:border-indigo-700  md:py-4 md:text-lg md:px-10"
             >
               View Case Studies
             </a>
           </div>
           <div className="w-full lg:w-1/2">
             <blockquote className="flex items-center justify-between w-full col-span-1 p-6 bg-white rounded-lg shadow">
-              <div className="flex flex-col pr-8">
-                <div className="relative pl-12">
+              <div className="flex flex-col w-9/12">
+                <div className="relative">
                   <svg
-                    className="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                    className="absolute -left-4 -top-4 w-10 h-10 text-indigo-500 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 100 125"
                   >
                     <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
                   </svg>
                   <p className="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
-                    Awesome product! And the customer service is exceptionally
-                    well.
+                    This SaaS is a game-changer for our workflow. The landing
+                    pages we've created are not just visually appealing but have
+                    significantly boosted our conversion rates. The fact that
+                    the support feels genuinely human and responsive makes all
+                    the difference!
                   </p>
                 </div>
 
-                <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
+                <h3 className="mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
                   Jane Cooper
-                  <span className="mt-1 text-sm leading-5 text-gray-500 truncate">
-                    - CEO SomeCompany
+                  <span className="whitespace-nowrap	mt-1 text-sm leading-5 text-gray-500 truncate">
+                    - Head of Operations, Synergy Solutions
                   </span>
                 </h3>
               </div>
@@ -53,10 +56,10 @@ const Testinomials = () => {
               />
             </blockquote>
             <blockquote className="flex items-center justify-between w-full col-span-1 p-6 mt-4 bg-white rounded-lg shadow">
-              <div className="flex flex-col pr-10">
-                <div className="relative pl-12">
+              <div className="flex flex-col w-9/12">
+                <div className="relative">
                   <svg
-                    className="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                    className="absolute -left-4 -top-4 w-10 h-10 text-indigo-500 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 100 125"
                   >
@@ -67,10 +70,10 @@ const Testinomials = () => {
                     for my company.
                   </p>
                 </div>
-                <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
+                <h3 className="mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
                   John Doe
                   <span className="mt-1 text-sm leading-5 text-gray-500 truncate">
-                    - CEO SomeCompany
+                    - Dynamic Product Labs
                   </span>
                 </h3>
                 <p className="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
@@ -82,25 +85,25 @@ const Testinomials = () => {
               />
             </blockquote>
             <blockquote className="flex items-center justify-between w-full col-span-1 p-6 mt-4 bg-white rounded-lg shadow">
-              <div className="flex flex-col pr-10">
-                <div className="relative pl-12">
+              <div className="flex flex-col w-9/12">
+                <div className="relative">
                   <svg
-                    className="absolute left-0 w-10 h-10 text-indigo-500 fill-current"
+                    className="absolute -left-4 -top-4 w-10 h-10 text-indigo-500 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 100 125"
                   >
                     <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
                   </svg>
                   <p className="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
-                    I can't express enough, how amazing this service has been
-                    for my company.
+                    It's not just the features (which are awesome), but the way
+                    the team provides support is outstanding
                   </p>
                 </div>
 
-                <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
+                <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-base">
                   John Smith
                   <span className="mt-1 text-sm leading-5 text-gray-500 truncate">
-                    - CEO SomeCompany
+                    - Co-Founder, Nexus Innovations
                   </span>
                 </h3>
                 <p className="mt-1 text-sm leading-5 text-gray-500 truncate"></p>
