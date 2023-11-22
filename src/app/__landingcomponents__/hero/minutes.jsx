48d3d078dda1d666f9341824488f1b1823960f9b
rajat <rajat@rajatmacbook.local>
2023-11-22 18:00:33 +0530

    Time Formatting

--- src/app/__landingcomponents__/hero/minutes.jsx
+++ src/app/__landingcomponents__/hero/minutes.jsx
@@ -5,11 +5,17 @@ import { useEffect, useState } from "react";
 const Minutes = () => {
   const [time, setTime] = useState();
   useEffect(() => {
-    setTime(new Date(Date.now() + 5 * 60 * 1000).toLocaleTimeString());
+    setTime(
+      new Date(Date.now() + 5 * 60 * 1000).toLocaleTimeString(
+        navigator.language,
+        { hour: "2-digit", minute: "2-digit", hour12: true }
+      )
+    );
   }, []);
   return (
     <p className="text-lg font-medium leading-8 text-indigo-600/95">
-      Landing Page will be ready today at {time}
+      Landing Page will be ready today at{" "}
+      <strong className=" uppercase">{time}</strong>
     </p>
   );
 };
