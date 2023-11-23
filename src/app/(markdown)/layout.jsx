88a0880e61fc0b0fe573ede4cf83afb39910c027
rajat <rajat@rajatmacbook.local>
2023-11-23 10:09:02 +0530

    nav added in terms and markdown pages

--- src/app/(markdown)/layout.jsx
+++ src/app/(markdown)/layout.jsx
@@ -1,8 +1,12 @@
 import "../../markdown.css";
+import NavBar from "@/app/__landingcomponents__/navbar/varient-2";
+import Footer from "@/app/__landingcomponents__/footer/varient-3";
 const MarkdownLayout = ({ children }) => {
   return (
     <>
+      <NavBar />
       <div className="markdown container mx-auto p-5">{children}</div>
+      <Footer />
     </>
   );
 };
