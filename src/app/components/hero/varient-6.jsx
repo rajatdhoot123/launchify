c4b5fbe9ceb5cc81ba99758f3d9495da03a581a1
Rajat <rajatdhoot123@gmail.com>
2023-12-01 16:51:22 +0530

    Now Change Config File

--- src/app/components/hero/varient-6.jsx
+++ src/app/components/hero/varient-6.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               aria-hidden="true"
               class="ml-2 h-4 w-4"
