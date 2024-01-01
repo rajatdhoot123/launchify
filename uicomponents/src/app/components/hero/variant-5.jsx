21853521e33e27b251176179c1c9cb8fdd34b278
Rajat <rajatdhoot123@gmail.com>
2024-01-01 18:01:35 +0530

    Fixes

--- uicomponents/src/app/components/hero/variant-5.jsx
+++ uicomponents/src/app/components/hero/variant-5.jsx
@@ -1,55 +1,75 @@
 const Hero = () => {
   return (
-    <div className="container mx-auto max-w-4xl text-center flex flex-col justify-center items-center">
-      <div className="rounded-[40px] border uppercase backdrop-blur-sm text-xs text-primary font-medium md:mt-0 mb-4 py-2 px-4 border-primary/40 w-fit mx-auto text-center z-10">
-        FREE UNTIL YOUR FIRST SUBSCRIBER
-      </div>
-      <h1 className="text-4xl md:text-6xl font-bold md:leading-[72px] mt-6 max-w-3xl">
-        Productize yourself, <br />
-        earn more with what you're good at.
-      </h1>
-      <p className="mt-6 text-primary/80 text-lg md:text-xl mb-10 max-w-xl">
-        With Breeew, anyone can generate recurring income online. People need
-        your skills, let them subscribe to you. It's that easy.
-      </p>
-      <form className="flex flex-1 flex-col gap-6 items-center w-full">
-        <div className="flex md:flex-row flex-col items-center justify-center mt-6 w-full max-w-lg">
-          <div className="flex flex-1 flex-col w-full relative">
-            <div className="group flex items-center space-x-2 w-full rounded-md border-input px-3 py-2 focus-within:outline-none focus-within:border-primary/30 h-11 md:h-14 rounded-b-none border-0 border-t border-x md:rounded-r-none md:rounded-l-md md:border md:border-r-0 bg-background md:px-6">
-              <input
-                className="bg-transparent w-full ring-offset-background file:border-0 outline-none file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground text-base"
-                placeholder="Email address"
-                name="email"
-                value=""
-              />
-            </div>
-          </div>
-          <button
-            className="inline-flex items-center justify-center font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-accent text-accent-foreground hover:bg-accent/90 h-11 md:h-14 px-6 rounded-md text-base rounded-t-none md:rounded-l-none w-full md:w-fit md:rounded-r-md"
-            type="submit"
+    <div className="px-12 mx-auto max-w-7xl">
+      <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
+        <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+          <span>Start</span>{" "}
+          <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">
+            building a buzz
+          </span>{" "}
+          <span>around your product ?</span>
+        </h1>
+        <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
+          Start gaining the traction you've always wanted with our next-level
+          templates and designs. Crafted to help you tell your story.
+        </p>
+        <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
+          <a
+            href="#_"
+            className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-green-400 rounded-2xl sm:w-auto sm:mb-0"
+            target="_blank"
           >
-            Start now
+            Get Started
             <svg
+              className="w-4 h-4 ml-1"
               xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          <a
+            href="#_"
+            className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-2xl sm:w-auto sm:mb-0"
+            target="_blank"
+          >
+            Learn More
+            <svg
+              className="w-4 h-4 ml-1"
               fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
               stroke="currentColor"
-              aria-hidden="true"
-              className="ml-2 h-4 w-4"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                strokeWidth="2"
+                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
               ></path>
             </svg>
-          </button>
+          </a>
         </div>
-        <span className="text-xs text-primary/60">
-          Free until first subscriber - No credit card needed
-        </span>
-      </form>
+      </div>
+      <div className="w-full mx-auto mt-20 text-center md:w-10/12">
+        <div className="relative z-0 w-full mt-8">
+          <div className="relative overflow-hidden shadow-2xl">
+            <div className="flex items-center flex-none px-4 bg-green-400 rounded-b-none h-11 rounded-xl">
+              <div className="flex space-x-1.5">
+                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
+                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
+                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
+              </div>
+            </div>
+            <img src="https://cdn.devdojo.com/images/march2021/green-dashboard.jpg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- uicomponents/src/app/components/hero/variant-6.jsx
+++ uicomponents/src/app/components/hero/variant-6.jsx
@@ -1,76 +1,111 @@
 const Hero = () => {
   return (
-    <div className="px-12 mx-auto max-w-7xl">
-      <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-        <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
-          <span>Start</span>{" "}
-          <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">
-            building a buzz
-          </span>{" "}
-          <span>around your product ?</span>
+    <>
+      <section
+        className="text-center"
+        data-scroll-magic-target="phoneContainer"
+      >
+        <h1 className="lg:w-2/3 text-3xl md:text-[56px] font-bold leading-none mx-auto">
+          1-app for everything money
         </h1>
-        <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
-          Start gaining the traction you've always wanted with our next-level
-          templates and designs. Crafted to help you tell your story.
-        </p>
-        <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-          <a
-            href="#_"
-            className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-green-400 rounded-2xl sm:w-auto sm:mb-0"
-            target="_blank"
-          >
-            Get Started
-            <svg
-              className="w-4 h-4 ml-1"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
-            href="#_"
-            className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-2xl sm:w-auto sm:mb-0"
-            target="_blank"
-          >
-            Learn More
-            <svg
-              className="w-4 h-4 ml-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-              ></path>
-            </svg>
-          </a>
-        </div>
-      </div>
-      <div className="w-full mx-auto mt-20 text-center md:w-10/12">
-        <div className="relative z-0 w-full mt-8">
-          <div className="relative overflow-hidden shadow-2xl">
-            <div className="flex items-center flex-none px-4 bg-green-400 rounded-b-none h-11 rounded-xl">
-              <div className="flex space-x-1.5">
-                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
-                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
-                <div className="w-3 h-3 border-2 border-white rounded-full"></div>
-              </div>
+        <div
+          className="flex justify-center ml-[-8.5rem] mb-6 my-2"
+          id="toggle-animation"
+        >
+          <p className="md:text-2xl text-sm pr-1">Manage your money like you</p>
+          <div className="cubespinner animate-flipper pb-2">
+            <div className="face1 w-32 text-sm md:text-2xl md:w-64">
+              shop for clothes
             </div>
-            <img src="https://cdn.devdojo.com/images/march2021/green-dashboard.jpg" />
+            <div className="face2 w-32 text-sm md:text-2xl md:w-64">
+              chat with friends
+            </div>
+            <div className="face3 w-32 text-sm md:text-2xl md:w-64">
+              order food
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+        <a
+          target="_blank"
+          data-controller="download-link"
+          href="https://jptr.onelink.me/TOMp/81c06bec"
+          className="bg-red-500 text-white text-sm md:text-xl hover:bg-red-700 hover:text-sf60 border border-sf60 py-3 px-6 rounded-full inline-block"
+        >
+          Open Savings Account
+        </a>
+        <p className="lg:w-2/5 text-lg mx-auto mb-6 md:text-2xl leading-tight text-black60">
+          powered by{" "}
+          <img
+            className="inline w-12 h-auto my-2"
+            src="/logo.png"
+            alt="Federal Bank"
+          />
+        </p>
+      </section>
+      <style jsx>{`
+        @keyframes flip {
+          0% {
+            transform: rotateX(0deg);
+          }
+
+          30% {
+            transform: rotateX(120deg);
+          }
+
+          60% {
+            transform: rotateX(240deg);
+          }
+
+          100% {
+            transform: rotateX(360deg);
+          }
+        }
+        .animate-flipper {
+          animation: flip 4s ease-in-out infinite;
+        }
+        .cubespinner {
+          transform-style: preserve-3d;
+          transform-origin: 40px 40px 0;
+          text-align: left;
+        }
+        .cubespinner div {
+          position: absolute;
+          height: 5rem;
+          text-align: left;
+          font-weight: 600;
+        }
+        .cubespinner .face1 {
+          color: #fc7a69;
+          transform: translateZ(20px);
+          -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+          -webkit-transform: translateZ(20px);
+          -moz-transform: translateZ(20px);
+          -ms-transform: translateZ(20px);
+          -o-transform: translateZ(20px);
+        }
+        .cubespinner .face2 {
+          color: #fc7a69;
+          transform: rotateX(120deg) translateZ(20px);
+          -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+          -webkit-transform: rotateX(120deg) translateZ(20px);
+          -moz-transform: rotateX(120deg) translateZ(20px);
+          -ms-transform: rotateX(120deg) translateZ(20px);
+          -o-transform: rotateX(120deg) translateZ(20px);
+        }
+        .cubespinner .face3 {
+          color: #fc7a69;
+          transform: rotateX(240deg) translateZ(20px);
+          -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+          -webkit-transform: rotateX(240deg) translateZ(20px);
+          -moz-transform: rotateX(240deg) translateZ(20px);
+          -ms-transform: rotateX(240deg) translateZ(20px);
+          -o-transform: rotateX(240deg) translateZ(20px);
+        }
+      `}</style>
+    </>
   );
 };
 
