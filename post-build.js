065e596fa573a0441d92b38739251265e556b8e0
Rajat <rajatdhoot123@gmail.com>
2024-01-05 17:02:46 +0530

    Now With Public Folder

--- post-build.js
+++ post-build.js
@@ -1,16 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-function isImageFile(fileName) {
-  const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "svg"]; // Add more extensions if needed
-
-  // Get the file extension from the fileName
-  const fileExtension = fileName.split(".").pop().toLowerCase();
-
-  // Check if the file extension is in the list of image extensions
-  return imageExtensions.includes(fileExtension);
-}
-
 fs.cpSync(
   path.join(process.cwd(), "src"),
   path.join(process.cwd(), "uicomponents", "src"),
@@ -22,5 +12,17 @@ fs.cpSync(
 fs.cpSync(
   path.join(process.cwd(), "public"),
   path.join(process.cwd(), "uicomponents", "public"),
-  { recursive: true, filter: (file) => isImageFile(file) }
+  {
+    recursive: true,
+    filter: (file, type, d) => {
+      if (
+        file.includes("__") ||
+        file.endsWith(".xml") ||
+        file.endsWith(".txt")
+      ) {
+        return false;
+      }
+      return true;
+    },
+  }
 );
