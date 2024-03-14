470209027f988d8814e6cecae0377911a5e640e2
rajat <rajat@rajatmacbook.local>
2024-03-14 10:02:47 +0530

    UI Fixes

--- src/app/components/__login_dialog/login.jsx
+++ src/app/components/__login_dialog/login.jsx
@@ -17,36 +17,22 @@ export function LoginDialog({ text }) {
         <Button className="cursor-pointer">{text}</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div
-          className="w-full absolute top-1/2 -translate-y-1/2 flex items-center justify-center"
-          data-id="1"
-        >
+        <div>
+          <h1 className="text-2xl font-bold text-center mb-5" data-id="4">
+            Login
+          </h1>
+          <GoogleButton />
           <div
-            className="bg-background drop-shadow-md	 rounded px-8 pt-6 pb-8 mb-4 max-w-md w-full overflow-hidden relative"
-            data-id="2"
+            className="border-b border-gray-200 dark:border-gray-600 my-4"
+            data-id="7"
+          ></div>
+          <p
+            className="mb-6 text-sm text-gray-600 dark:text-gray-400"
+            data-id="8"
           >
-            <h1 className="text-2xl font-bold text-center mb-5" data-id="4">
-              Login
-            </h1>
-            <GoogleButton />
-            <div
-              className="border-b border-gray-200 dark:border-gray-600 my-4"
-              data-id="7"
-            ></div>
-            <p
-              className="mb-6 text-sm text-gray-600 dark:text-gray-400"
-              data-id="8"
-            >
-              Or get a magic link sent to your email
-            </p>
-            <EmailSignIn />
-          </div>
+            Or get a magic link sent to your email
+          </p>
+          <EmailSignIn />
         </div>
       </DialogContent>
     </Dialog>
