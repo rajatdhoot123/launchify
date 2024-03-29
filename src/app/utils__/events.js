f30c24ab27b6a3ded8736713cd299ca6d7db0995
rajat <rajat@rajatmacbook.local>
2024-03-29 15:09:55 +0530

    PostHog Events added

--- src/app/utils__/events.js
+++ src/app/utils__/events.js
@@ -1,4 +1,12 @@
 export const logEvent = (eventname, params = {}) => {
+  if (typeof window?.poshog !== "undefined") {
+    try {
+      window.posthog.capture(eventname, params);
+    } catch (error) {
+      console.error("Error logging event", error);
+    }
+    return;
+  }
   if (typeof window !== "undefined" && typeof window.gtag !== "undefined") {
     window.gtag("event", eventname, params);
   }
