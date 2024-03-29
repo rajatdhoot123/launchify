fb680241ee8ec6d379e4f11dcb5ed6249e0bbcd7
rajat <rajat@rajatmacbook.local>
2024-03-29 15:08:12 +0530

    Post Hog added

--- src/app/landingcomponent/pricing/index.jsx
+++ src/app/landingcomponent/pricing/index.jsx
@@ -34,7 +34,7 @@ const Pricing = () => {
                       className="text-black text-opacity-80 h-4 w-4 mr-0.5"
                       stroke="currentColor"
                       fill="currentColor"
-                      stroke-width="0"
+                      strokeWidth="0"
                       viewBox="0 0 512 512"
                       xmlns="http://www.w3.org/2000/svg"
                     >
