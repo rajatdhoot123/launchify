c49f41eb0560f936085aaaf2d27804c24873693d
rajat <rajat@rajatmacbook.local>
2024-04-20 10:38:51 +0530

    Delete React DND

--- src/app/app__/page.js
+++ src/app/app__/page.js
@@ -1,13 +1,10 @@
 "use client";
-import Floater from "@/app/components/__floater/variant-1";
-import { createRef, useEffect, useRef, useState } from "react";
+import { createRef, useRef, useState } from "react";
 import {
   FLOATER_SELECT,
   PAGES,
   PREMIUM_FEATURES,
 } from "@/app/constants__/floater";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { Button } from "@/components/ui/button";
 
 const LeftDrawer = ({ children = null, is_active }) => {
@@ -36,54 +33,35 @@ export default function Home() {
 
   const elementsRef = useRef(state.components.map(() => createRef()));
 
-  const handleShowCode = (index) => {
-    return elementsRef.current[index].current.innerHTML;
-  };
-
   const toggleHamburger = () => {
     set_is_active((prev) => !prev);
   };
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        {!is_active && (
-          <Button
-            onClick={toggleHamburger}
-            className="z-20 bottom-10 fixed right-10 bg-primary text-primary-foreground shadow hover:bg-primary/90 rounded-full h-16 w-16"
+      {!is_active && (
+        <Button
+          onClick={toggleHamburger}
+          className="z-20 bottom-10 fixed right-10 bg-primary text-primary-foreground shadow hover:bg-primary/90 rounded-full h-16 w-16"
+        >
+          <svg
+            className="text-xl md:text-2xl"
+            stroke="currentColor"
+            fill="none"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              className="text-xl md:text-2xl"
-              stroke="currentColor"
-              fill="none"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
-              <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
-              <path d="M16 5l3 3"></path>
-            </svg>
-          </Button>
-        )}
-        <LeftDrawer is_active={is_active}>
-          <Floater
-            toggleHamburger={toggleHamburger}
-            handleShowCode={handleShowCode}
-            pages={state.pages}
-            crisp_id={state.crisp_id}
-            premium_features={state.premium_features}
-            ga_id={state.ga_id}
-            components={state.components}
-            setState={setState}
-          />
-        </LeftDrawer>
-      </DndProvider>
-
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+            <path d="M16 5l3 3"></path>
+          </svg>
+        </Button>
+      )}
       <div className="space-y-24">
         {state.components.map((comp, index) => {
           const Component = comp.variants[comp.selected];
