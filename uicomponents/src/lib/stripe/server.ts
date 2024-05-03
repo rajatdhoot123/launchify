ff1c82936f45b89d840045aaaa9a6c5937f69b65
rajat <rajat@rajatmacbook.local>
2024-05-03 13:54:42 +0530

    Update Stripe Page

--- uicomponents/src/lib/stripe/server.ts
+++ uicomponents/src/lib/stripe/server.ts
@@ -6,7 +6,7 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2023-10-16",
   appInfo: {
-    name: "nextjs-with-stripe-typescript-demo",
-    url: "https://nextjs-with-stripe-typescript-demo.vercel.app",
+    name: "launchify.club",
+    url: "https://www.launchify.club",
   },
 });
