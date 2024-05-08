8ded54cc78086a7c33c5d03c24ccadd09b2e7944
rajat <rajat@rajatmacbook.local>
2024-05-08 11:56:16 +0530

    Now Import multiple components

--- src/app/utils__/generateRootPage.js
+++ src/app/utils__/generateRootPage.js
@@ -2,15 +2,31 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function updateItemId(arr) {
+  const itemCount = {};
+  return arr.map((item) => {
+    const key = item.key;
+    if (!itemCount[key]) itemCount[key] = 0;
+    itemCount[key]++;
+
+    return {
+      ...item,
+      updated_item_id: `${key}${itemCount[key] > 1 ? itemCount[key] : ""}`,
+    };
+  });
+}
+
 const generateRootPage = ({ components, copywriting_components = [] }) => {
+  const modified_component = updateItemId(components);
+
   return `
 
-${components.reduce((acc, { item_id, variant }) => {
+${modified_component.reduce((acc, { item_id, variant, updated_item_id }) => {
   const select_comp_index = copywriting_components.findIndex(
     (file) => file.item_id === item_id
   );
   return `${acc} \n import ${capitalizeFirstLetter(
-    item_id
+    updated_item_id
   )} from "@/app/components/${item_id}/${
     select_comp_index === -1 ? variant : "index"
   }";`;
@@ -20,8 +36,8 @@ export default function Home() {
       return (
         <>
           <div className="w-full overflow-scroll space-y-12">
-          ${components.reduce((acc, { item_id }) => {
-            return `${acc} \n <${capitalizeFirstLetter(item_id)} />`;
+          ${modified_component.reduce((acc, { updated_item_id }) => {
+            return `${acc} \n <${capitalizeFirstLetter(updated_item_id)} />`;
           }, "")}
           </div>
         </>
