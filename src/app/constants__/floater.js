0977601abdd950111ef2a0f4f1d70213d2175fd4
Rajat <rajatdhoot123@gmail.com>
2024-06-02 22:17:45 +0530

    More Code Removed

--- src/app/constants__/floater.js
+++ src/app/constants__/floater.js
@@ -27,8 +27,8 @@ import Card8 from "@/app/components/cards/variant-8";
 import Card9 from "@/app/components/cards/variant-9";
 import Card10 from "@/app/components/cards/variant-10";
 import Card11 from "@/app/components/cards/variant-11";
-import RandomCard1 from "@/app/components/randomcards/variant-1";
-import RandomCard2 from "@/app/components/randomcards/variant-2";
+// import RandomCard1 from "@/app/components/randomcards/variant-1";
+// import RandomCard2 from "@/app/components/randomcards/variant-2";
 import Footer1 from "@/app/components/footer/variant-1";
 import Footer2 from "@/app/components/footer/variant-2";
 import Footer3 from "@/app/components/footer/variant-3";
@@ -44,106 +44,6 @@ import About1 from "@/app/components/about/variant-1";
 import Feature1 from "@/app/components/featureservice/variant-1";
 import AnimatedComp1 from "@/app/components/animated/variant-1.jsx";
 import ChatBot1 from "@/app/ai/chatbot/page";
-
-const NAVBAR = {
-  "variant-1": Navbar1,
-  "variant-2": Navbar2,
-};
-
-const FEATURE_SERVICE = {
-  "variant-1": Feature1,
-};
-
-const CHATBOT = {
-  "variant-1": ChatBot1,
-};
-
-const ABOUT = {
-  "variant-1": About1,
-};
-
-const HERO = {
-  "variant-1": Hero1,
-  "variant-2": Hero2,
-  "variant-3": Hero3,
-  "variant-4": Hero4,
-  "variant-5": Hero5,
-};
-
-const SECTION = {
-  "variant-1": Section1,
-};
-
-const PRICING = {
-  "variant-1": Pricing1,
-  "variant-2": Pricing2,
-  "variant-3": Pricing3,
-  "variant-4": Pricing4,
-  "variant-5": Pricing5,
-  "variant-6": Pricing6,
-};
-
-const TESTIMONIALS = {
-  "variant-1": Testimonials1,
-  "variant-2": Testimonials2,
-};
-
-const TABLE = {
-  "variant-1": Table1,
-  "variant-2": Table2,
-  "variant-3": Table3,
-};
-
-const RANDOMCARD = {
-  "variant-1": RandomCard1,
-  "variant-2": RandomCard2,
-};
-
-const FOOTER = {
-  "variant-1": Footer1,
-  "variant-2": Footer2,
-  "variant-3": Footer3,
-  "variant-4": Footer4,
-};
-
-const FAQ = {
-  "variant-1": Faq1,
-};
-
-const CARDS = {
-  "variant-2": Card2,
-  "variant-3": Card3,
-  "variant-4": Card4,
-  "variant-5": Card5,
-  "variant-6": Card6,
-  "variant-7": Card7,
-  "variant-8": Card8,
-  "variant-9": Card9,
-  "variant-10": Card10,
-  "variant-11": Card11,
-};
-
-const COMMING_SOON = {
-  "variant-1": WaitList1,
-};
-
-const ANIMATED_CARDS = {
-  "variant-1": Animated1,
-};
-
-const ANIMATED_COMPS = {
-  "variant-1": AnimatedComp1,
-};
-
-const CONTACT_US = {
-  "variant-1": Contactus1,
-  "variant-2": Contactus2,
-};
-
-const LOGIN = {
-  "variant-1": Login1,
-  "variant-2": Login2,
-};
 
 export const COMPONENTS_ARRAY = [
   {
@@ -503,68 +403,5 @@ export const PAGES = [
     item_id: "privacy-policy",
     selected: false,
     title: "Privacy Policy",
-  },
-];
-
-export const FLOATER_SELECT = [
-  {
-    item_id: "navbar",
-    variants: NAVBAR,
-    title: "Navbar",
-    selected: "variant-2",
-  },
-  {
-    item_id: "hero",
-    variants: HERO,
-    title: "Hero",
-    selected: "variant-5",
-  },
-  {
-    item_id: "featureservice",
-    variants: FEATURE_SERVICE,
-    title: "Feature Service",
-    selected: "variant-1",
-  },
-  {
-    item_id: "about",
-    variants: ABOUT,
-    title: "About",
-    selected: "variant-1",
-  },
-  {
-    item_id: "pricing",
-    variants: PRICING,
-    title: "Pricing",
-    selected: "variant-3",
-  },
-  {
-    item_id: "section",
-    variants: SECTION,
-    title: "Sections",
-    selected: "variant-1",
-  },
-  {
-    item_id: "login",
-    variants: LOGIN,
-    title: "Login",
-    selected: "variant-2",
-  },
-  {
-    item_id: "testimonials",
-    variants: TESTIMONIALS,
-    title: "Testimonials",
-    selected: "variant-1",
-  },
-  {
-    item_id: "table",
-    variants: TABLE,
-    title: "Table",
-    selected: "variant-1",
-  },
-  {
-    item_id: "faq",
-    variants: FAQ,
-    title: "Faq",
-    selected: "variant-1",
   },
 ];
